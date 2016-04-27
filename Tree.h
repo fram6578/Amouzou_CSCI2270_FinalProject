ad7a2f673d9382c95f22cb4606cd6542de037bd9
fram6578 <fram6578@colorado.edu>
2016-04-27 04:12:27 -0600

    Tree header updated to include all relevent methods and variables

--- Tree.h
+++ Tree.h
@@ -1,14 +1,21 @@
 #ifndef TREE_H
 #define TREE_H
 #include <string>
-struct Employee{
+struct Employee{ //basic employee struct, includes name senior and juniors
 	std::string name="";
-	Employee *senior=NULL;
-	Employee *juniors=NULL;
+	int pay=0;
+	
+	Employee *senior=NULL;//points to senior
+	Employee *juniors=NULL;//points to head of linked list of juniors
+	
+	Employee *previous=NULL;//points to downranking co-worker
+	Employee *next=NULL;//points to upranking co-worker
+	
 	Employee(){};
-	Employee(std::string in_name, Employee* in_senior){
+	Employee(std::string in_name, int in_pay, Employee* in_senior){//intializes employee with name and senior
 		name=in_name;
 		senior=in_senior;
+		pay=in_pay;
 	}
 };
 
@@ -17,8 +24,23 @@ class Tree
 	public:
 		Tree();
 		~Tree();
+		
+		void findEmployee(std::string name);//finds and displays information of employee
+		void addEmployee(std::string name, int salary, std::string senior, std::string uprank);//adds employee to tree
+		void replaceEmployee(std::string name, std::string replacement);//removes employee from tree and replaces them
+		void promoteEmployee(std::string name);//moves employee up one step in tree
+		void modifyEmployee(std::string name, int salary, std::string uprank);//changes employee's rank and/or salary
+		void shiftEmployees(std::string name, std::junior);//moves junior employees to another senior.
+		
+		void listEmployees();//prints out all employees in list
+		void printEmployees();//prints out all employees in tree
+		void countEmployees();//prints total number of employees and maybe other relevent data
+		
+		
 	protected:
 	private:
+		Employee *boss;//root of employee tree
+		Employee *search(std::string name);//find pointer to specific employee
 };
 
 #endif
