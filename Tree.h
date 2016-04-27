636a54ad2e32b6f4d39597976f5db7c577aad141
fram6578 <fram6578@colorado.edu>
2016-04-27 03:21:39 -0600

    Tree header updated to include Employee struct, initial constructor and destructor added

--- Tree.h
+++ Tree.h
@@ -1,9 +1,22 @@
 #ifndef TREE_H
 #define TREE_H
+#include <string>
+struct Employee{
+	std::string name="";
+	Employee *senior=NULL;
+	Employee *juniors=NULL;
+	Employee(){};
+	Employee(std::string in_name, Employee* in_senior){
+		name=in_name;
+		senior=in_senior;
+	}
+};
 
 class Tree
 {
 	public:
+		Tree();
+		~Tree();
 	protected:
 	private:
 };
